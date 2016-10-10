1f8303ae4ad0bff6f9f3fb8577b12be4ee6593b4
hc2014 <294836881@qq.com>
2016-10-10 16:46:51 +0800

    a

--- RedisFor.net/Redis/Redis.Designer.cs
+++ RedisFor.net/Redis/Redis.Designer.cs
@@ -28,105 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSetting = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnGet = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnCommit = new System.Windows.Forms.Button();
+            this.btnRoleBack = new System.Windows.Forms.Button();
+            this.btnErrorCommit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnSetting
+            // groupBox1
             // 
-            this.btnSetting.Location = new System.Drawing.Point(233, 61);
-            this.btnSetting.Name = "btnSetting";
-            this.btnSetting.Size = new System.Drawing.Size(75, 23);
-            this.btnSetting.TabIndex = 0;
-            this.btnSetting.Text = "设置";
-            this.btnSetting.UseVisualStyleBackColor = true;
-            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnSetting);
+            this.groupBox1.Location = new System.Drawing.Point(36, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "设置";
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(76, 58);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "值:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 61);
+            this.label1.Location = new System.Drawing.Point(13, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(23, 12);
-            this.label1.TabIndex = 2;
+            this.label1.TabIndex = 9;
             this.label1.Text = "键:";
             // 
-            // btnGet
+            // textBox1
             // 
-            this.btnGet.Location = new System.Drawing.Point(26, 132);
-            this.btnGet.Name = "btnGet";
-            this.btnGet.Size = new System.Drawing.Size(75, 23);
-            this.btnGet.TabIndex = 3;
-            this.btnGet.Text = "获取";
-            this.btnGet.UseVisualStyleBackColor = true;
-            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            this.textBox1.Location = new System.Drawing.Point(36, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.Location = new System.Drawing.Point(165, 25);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(75, 23);
+            this.btnSetting.TabIndex = 7;
+            this.btnSetting.Text = "设置";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            this.btnSetting.Click += new System.EventHandler(this.btnSetting_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.btnGet);
+            this.groupBox2.Location = new System.Drawing.Point(36, 132);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(156, 206);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "获取";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(26, 161);
+            this.listBox1.Location = new System.Drawing.Point(6, 52);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 148);
-            this.listBox1.TabIndex = 4;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.TabIndex = 6;
             // 
-            // textBox2
+            // btnGet
             // 
-            this.textBox2.Location = new System.Drawing.Point(76, 87);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 5;
+            this.btnGet.Location = new System.Drawing.Point(6, 20);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.TabIndex = 5;
+            this.btnGet.Text = "获取";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
             // 
-            // label2
+            // groupBox3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 90);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(23, 12);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "值:";
+            this.groupBox3.Controls.Add(this.btnErrorCommit);
+            this.groupBox3.Controls.Add(this.btnRoleBack);
+            this.groupBox3.Controls.Add(this.btnCommit);
+            this.groupBox3.Location = new System.Drawing.Point(199, 133);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(381, 205);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "事物";
+            // 
+            // btnCommit
+            // 
+            this.btnCommit.Location = new System.Drawing.Point(16, 20);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(90, 23);
+            this.btnCommit.TabIndex = 0;
+            this.btnCommit.Text = "正常执行事物";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
+            // btnRoleBack
+            // 
+            this.btnRoleBack.Location = new System.Drawing.Point(134, 19);
+            this.btnRoleBack.Name = "btnRoleBack";
+            this.btnRoleBack.Size = new System.Drawing.Size(90, 23);
+            this.btnRoleBack.TabIndex = 1;
+            this.btnRoleBack.Text = "回滚事物";
+            this.btnRoleBack.UseVisualStyleBackColor = true;
+            this.btnRoleBack.Click += new System.EventHandler(this.btnRoleBack_Click);
+            // 
+            // btnErrorCommit
+            // 
+            this.btnErrorCommit.Location = new System.Drawing.Point(16, 51);
+            this.btnErrorCommit.Name = "btnErrorCommit";
+            this.btnErrorCommit.Size = new System.Drawing.Size(113, 23);
+            this.btnErrorCommit.TabIndex = 2;
+            this.btnErrorCommit.Text = "报错但是正常执行事物";
+            this.btnErrorCommit.UseVisualStyleBackColor = true;
+            this.btnErrorCommit.Click += new System.EventHandler(this.btnErrorCommit_Click);
             // 
             // Redis
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 331);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.btnGet);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnSetting);
+            this.ClientSize = new System.Drawing.Size(592, 435);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Redis";
             this.Text = "Redis";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnSetting;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnGet;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCommit;
+        private System.Windows.Forms.Button btnRoleBack;
+        private System.Windows.Forms.Button btnErrorCommit;
+
     }
 }
 
--- RedisFor.net/Redis/Redis.cs
+++ RedisFor.net/Redis/Redis.cs
@@ -17,6 +17,13 @@ namespace Redis
             InitializeComponent();
         }
 
+        RedisClient client = new RedisClient("127.0.0.1", 6379);
+
+        /// <summary>
+        /// 设置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSetting_Click(object sender, EventArgs e)
         {
             if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "")
@@ -24,14 +31,18 @@ namespace Redis
                 MessageBox.Show("请输入键值对!");
                 return;
             }
-            RedisClient client = new RedisClient("127.0.0.1", 6379);
+            
             client.Set<string>(textBox1.Text, textBox2.Text);
         }
 
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnGet_Click(object sender, EventArgs e)
         {
             listBox1.Items.Clear();
-            RedisClient client = new RedisClient("127.0.0.1", 6379);
             List<string> list=client.GetAllKeys();
             foreach (string item in list)
             {
@@ -44,6 +55,70 @@ namespace Redis
         {
             textBox1.Text = listBox1.Text.Split(' ')[0].Split(':')[1];
             textBox2.Text = listBox1.Text.Split(' ')[1].Split(':')[1];
+        }
+
+        /// <summary>
+        /// 正常执行事物
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCommit_Click(object sender, EventArgs e)
+        {
+            using (IRedisTransaction IRT = client.CreateTransaction())
+            {
+                //设置一个key=key  value=20的数据
+                IRT.QueueCommand(r => r.Set("key", 20));
+                //然后让该数据++1
+                IRT.QueueCommand(r => r.Increment("key", 1));
+
+                IRT.Commit(); // 提交事务
+                //最终该变量的值是21
+            }
+            btnGet_Click(null, null);
+        }
+
+        private void btnRoleBack_Click(object sender, EventArgs e)
+        {
+            //先检测key=key的变量
+            client.Watch("key");
+            //然后再事物之前修改该变量，
+            client.Set("key", "123");
+            using (IRedisTransaction IRT = client.CreateTransaction())
+            {
+                //设置一个key=key  value=20的数据
+                IRT.QueueCommand(r => r.Set("key", 20));
+                //然后让该数据++1
+                IRT.QueueCommand(r => r.Increment("key", 1));
+
+                IRT.Commit(); // 提交事务
+                //事物在操作该变量的时候，发现key这个变量已经发生改变了，所以就自动回滚了,值还是之前设置的123
+            }
+            btnGet_Click(null, null);
+        }
+
+        private void btnErrorCommit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (IRedisTransaction IRT = client.CreateTransaction())
+                {
+                    //设置一个key=key  value=aaa的数据
+                    IRT.QueueCommand(r => r.Set("key", "aaa"));
+                    //然后让该数据++1,但是字符串++1是会报错的，所以这一句是没有执行成功的，但是第一句执行成功了
+                    IRT.QueueCommand(r => r.Increment("key", 1));
+
+                    IRT.Commit(); // 提交事务
+                    //最终该变量的值还是aaa
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                btnGet_Click(null, null);
+            }
         }
 
         
